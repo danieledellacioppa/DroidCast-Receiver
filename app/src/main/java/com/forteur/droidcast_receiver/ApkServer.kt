e307dfe02d8cc7e96747ccefed2ba645ea26f646
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2025-01-18 18:38:38 +0000

    I can download using the URL in a browser but not safely

--- app/src/main/java/com/forteur/droidcast_receiver/ApkServer.kt
+++ app/src/main/java/com/forteur/droidcast_receiver/ApkServer.kt
@@ -7,15 +7,16 @@ import java.io.File
 
 class ApkServer(private val context: Context, port: Int,  private val apkFile: File) : NanoHTTPD(port) {
     override fun serve(session: IHTTPSession): Response {
-//        val file = File(context.getExternalFilesDir(null), "raw/droidcast_projector.apk")
         return if (apkFile.exists()) {
             Log.d("ApkServer", "Serving APK file")
-            newFixedLengthResponse(
+            val fis = apkFile.inputStream()
+            val response = newChunkedResponse(
                 Response.Status.OK,
                 "application/vnd.android.package-archive",
-                apkFile.inputStream(),
-                apkFile.length()
+                fis
             )
+            response.addHeader("Content-Disposition", "attachment; filename=\"droidcast_projector.apk\"")
+            response
         } else {
             Log.e("ApkServer", "APK file not found")
             newFixedLengthResponse(
