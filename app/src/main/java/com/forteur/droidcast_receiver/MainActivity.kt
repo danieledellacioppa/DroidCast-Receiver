55ee08be70dde6125bd35db7027442cab30dc42e
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2025-01-22 01:00:54 +0000

    testing new apk

--- app/src/main/java/com/forteur/droidcast_receiver/MainActivity.kt
+++ app/src/main/java/com/forteur/droidcast_receiver/MainActivity.kt
@@ -111,7 +111,7 @@ class MainActivity : ComponentActivity() {
                             )
                             Spacer(modifier = Modifier.height(16.dp))
                             Text(
-                                text = "Receiver IP: $ipAddress",
+                                text = "New Receiver IP: $ipAddress",
                                 textAlign = TextAlign.Center,
                                 style = TextStyle(
                                     color = Color.Yellow,
